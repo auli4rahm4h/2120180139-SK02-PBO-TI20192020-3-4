98c24c8114f0b41b39ce8ebbad87a2edfd5887ea
auli4rahm4h <55832890+auli4rahm4h@users.noreply.github.com>
2019-10-02 23:19:40 -0700

    upload tugas studi kasus 2

---
+++ 2120180139-SK02-PBO-TI20192020-3-4-02/src/sk02/pbo/ti20192020/pkg3/pkg4/pkg02/aulia/Makeup.java
@@ -0,0 +1,12 @@
+package sk02.pbo.ti20192020.pkg3.pkg4.pkg02.aulia;
+public class Makeup {
+    String nama="Makeup";
+    int bedak;
+    int gincu;
+    void mempercantik(){
+        System.out.println("singo"+ this.nama);
+    }
+    void memperindah(){
+        System.out.println("yo");
+    }
+}
---
+++ 2120180139-SK02-PBO-TI20192020-3-4-02/src/sk02/pbo/ti20192020/pkg3/pkg4/pkg02/aulia/SK02PBOTI201920203402Aulia.java
@@ -0,0 +1,15 @@
+package sk02.pbo.ti20192020.pkg3.pkg4.pkg02.aulia;
+public class SK02PBOTI201920203402Aulia {
+
+    public static void main(String[] args) {
+    Makeup Makeup = new Makeup();
+        Makeup.gincu=100;        
+        Makeup.bedak=10;                
+        System.out.println(Makeup.nama);
+        System.out.println("Chat anda sekarang "+Makeup.gincu);
+        System.out.println("Anda tidak menjawab "+Makeup.bedak);
+        Makeup.mempercantik();
+        Makeup.memperindah();
+    }
+    
+}
